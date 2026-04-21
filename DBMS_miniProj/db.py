4bb839b856994830467aca3e1703c9d329f3473b
Sumanyu Seth <140434264+Sumanyu-seth@users.noreply.github.com>
2026-04-21 11:29:50 +0530

    Remove sensitive password from DB configuration
    Removed the database password for security reasons.

--- DBMS_miniProj/db.py
+++ DBMS_miniProj/db.py
@@ -3,7 +3,7 @@ import mysql.connector
 DB_CONFIG = {
     "host": "localhost",
     "user": "root",
-    "password": "Sumanyu#seth",
+    "password": "",
     "database": "crimedb",
     "raise_on_warnings": True,
     "autocommit": False
